60ca62798b92de62b7d7f9d0dbb84f57111b6244
Marcus Herrera <maherreraiv1987@gmail.com>
2022-05-16 08:54:36 -0600

    added trip routes

--- routes/api/tripRoutes.js
+++ routes/api/tripRoutes.js
@@ -2,11 +2,31 @@ const router = require('express').Router();
 const { Traveller, Trip, Location } = require('../../models');
 
 router.post('/', async (req, res) => {
-
+    try {
+        const tripData = await Trip.create(req.body);
+        res.status(200).json(tripData)
+    } catch (error) {
+        res.status(500).json(error)
+    }
 })
 
 router.delete('/:id', async (req, res) => {
+    try {
+        const tripData = Trip.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
 
+        if (!tripData) {
+            res.status(404).json({ message: 'No trip with this id' });
+            return;
+        }
+
+        res.status(200).json(tripData)
+    } catch (error) {
+        res.status(500).json(error)
+    }
 })
 
 module.exports = router;
